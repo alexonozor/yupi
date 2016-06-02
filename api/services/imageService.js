4b94b3c4413c7e65b68dadb640e77317967689d5
alexonozor <alexonozor@gmail.com>
2016-06-02 13:37:38 +0100

    remove the undefind console log

--- api/services/imageService.js
+++ api/services/imageService.js
@@ -1,6 +1,6 @@
 var cloudinary = require('cloudinary');
 cloudinary.config({
-  cloud_name: sails.config.cloudinaryKey.cloud_name || process.env.cloud_name,
+  cloud_name: process.env.cloud_name,
   api_key:    sails.config.cloudinaryKey.api_key || process.env.api_key,
   api_secret: sails.config.cloudinaryKey.api_secret || process.env.api_secret
 });
